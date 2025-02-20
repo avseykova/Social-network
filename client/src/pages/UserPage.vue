67ee400e221eec09e2a689bc25442d570595c8c0
Avseykova Yuliya <avsejkovaulia@gmail.com>
2025-02-20 18:12:17 +0300

    added user icon with editing option

--- client/src/pages/UserPage.vue
+++ client/src/pages/UserPage.vue
@@ -2,15 +2,21 @@
 import { ref, onMounted } from "vue";
 import { API_BASE_URL } from "../config/config.ts";
 import { strings } from "../resources/strings.ts";
-import type { IUserInfoResponse } from '../models/userInfoResponse.ts';
 import { USER_KEY } from "../utils/constants.ts";
 import { navigateTo } from "../router/routerService.ts";
 import { Pages } from "../utils/pages.ts";
+import axios from "axios";
 
 const name = ref<string>('');
 const email = ref<string>('');
 const message = ref<string>('');
 const isError = ref<boolean>(false);
+const vOndialog = ref<boolean>(false);
+const avatarUrl = ref<string>(
+  'http://localhost:5002/uploads/default_avatar.png'
+);
+const selectedFile = ref<File | null>(null);
+const isUploading = ref<boolean>(false);
 
 const vOnhandleLogout = () => {
   localStorage.removeItem(USER_KEY);
@@ -28,31 +34,81 @@ const fetchUser = async (): Promise<void> => {
       navigateTo(Pages.Login);
       return;
     }
+    const response = await axios.post(
+      `${API_BASE_URL}/user-info`,
+      { username },
+      {
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
 
-    const response = await fetch(`${API_BASE_URL}/user-info`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: username }),
-    });
-
-    const data: IUserInfoResponse = await response.json();
-
-    name.value = data.username;
-    email.value = data.email;
-
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    if (response.data) {
+      name.value = response.data.username;
+      email.value = response.data.email;
+      avatarUrl.value =
+        response.data.avatarUrl ||
+        'http://localhost:5002/uploads/default_avatar.png';
     }
 
     message.value = strings.userPageLoaded;
   } catch (error) {
     message.value =
-    error instanceof Error ? error.message : strings.networkError;
+      error instanceof Error ? error.message : strings.networkError;
     isError.value = true;
   }
 };
 
-onMounted(() => {fetchUser()});
+const updateUserBD = async (username: string, avatarUrl: string) => {
+  try {
+    const response = await axios.put('http://localhost:5002/updateuser', {
+      username,
+      avatarUrl,
+    });
+
+    console.log('Аватар успешно обновлён:', response.data.message);
+  } catch (error: any) {
+    console.error('Ошибка:', error.response?.data?.error || error.message);
+  }
+};
+
+const vOnFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files[0]) {
+    selectedFile.value = target.files[0];
+    uploadAvatar();
+  }
+};
+
+const uploadAvatar = async () => {
+  if (!selectedFile.value) return;
+  isUploading.value = true;
+
+  const formData = new FormData();
+  formData.append('avatar', selectedFile.value);
+
+  try {
+    const response = await axios.post(
+      'http://localhost:5002/profile/upload-avatar',
+      formData,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }
+    );
+
+    avatarUrl.value = `http://localhost:5002${response.data.url}`;
+
+    await updateUserBD(name.value, avatarUrl.value);
+  } catch (error) {
+    console.error('Ошибка загрузки:', error);
+  } finally {
+    isUploading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchUser();
+});
+
 </script>
 
 <template>
@@ -60,11 +116,39 @@ onMounted(() => {fetchUser()});
     <v-card class="pa-6" width="300">
       <v-card-title class="text-center text-h5">User Page</v-card-title>
 
+      <v-card-text class="d-flex flex-column align-center">
+        <v-hover v-slot:default="{ isHovering, props }">
+          <div class="image-container" v-bind="props">
+            <v-img
+              class="rounded"
+              v-if="avatarUrl"
+              :src="avatarUrl"
+              height="200"
+              width="300"
+              cover
+            >
+              <v-fade-transition>
+                <v-btn
+                  class="edit-btn"
+                  v-if="isHovering"
+                  color="primary"
+                  icon
+                  @click="vOndialog = true"
+                >
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </v-fade-transition>
+            </v-img>
+          </div>
+        </v-hover>
+      </v-card-text>
+
       <v-card-text>
         <v-alert
           class="mb-4"
           v-if="message"
-          :type="isError ? 'error' : 'success'">
+          :type="isError ? 'error' : 'success'"
+        >
           {{ message }}
         </v-alert>
 
@@ -84,5 +168,23 @@ onMounted(() => {fetchUser()});
         </v-btn>
       </v-card-text>
     </v-card>
+
+    <v-dialog v-model="vOndialog" max-width="400">
+      <v-card>
+        <v-card-title>Загрузить новое изображение</v-card-title>
+        <v-card-text>
+          <v-file-input
+            accept="image/*"
+            label="Выберите изображение"
+            @change="vOnFileChange"
+          ></v-file-input>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red" @click="vOndialog = false">Отмена</v-btn>
+          <v-btn color="primary" @click="vOndialog = false">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
